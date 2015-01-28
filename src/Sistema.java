40702e0df84b6ef67c3afccee31064ccacbbd447
othman853 <anuar.yasser@gmail.com>
2015-01-28 13:55:13 -0200

    Marcus - Subindo o projeto IndividualC com a classe Main e Sistema

---
+++ src/Sistema.java
@@ -0,0 +1,27 @@
+
+public class Sistema {
+	
+	private String mensagem = "Bem-vindo à Livraria Sofia";
+	
+	public Sistema() {
+		// TODO Auto-generated constructor stub
+		
+		this.mensagem = mensagem;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	public String imprimeMensagem(){
+		
+		return mensagem;
+	}
+	
+	
+
+}
